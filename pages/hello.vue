9a69e9398d9c67445e380afecdbf1024fc952cc1
Subilan <suyuanliang1995@gmail.com>
2024-05-07 23:07:03 +0800

    partial

--- pages/hello.vue
+++ pages/hello.vue
@@ -1,38 +1,49 @@
 <template>
-    <div class="container">
-        <div class="hello-card">
-            <div class="textaligncenter">
-                <h1>
-                    👋 欢迎来到 <em>TiLab</em>！
-                </h1>
-                <p>TiLab 是 Seati 为玩家搭建的一站式服务器信息获取与控制平台 ✨</p>
-            </div>
-            
-            <div class="apply">
-                <btn>立即体验 <icon :path="mdiCreationOutline"/></btn>
-                <btn href="https://seati.cc">打开官网 <icon :path="mdiLaunch"/></btn>
-            </div>
+  <div class="container">
+    <div class="hello-card">
+      <div class="textaligncenter">
+        <h1>
+          👋 欢迎来到 <em>TiLab</em>！
+        </h1>
+        <p>TiLab 是 Seati 为玩家搭建的一站式服务器信息获取与控制平台 ✨</p>
+      </div>
 
-            <section class="section__here_you_can">
-                <h2>免费、灵活、开放</h2>
-            </section>
-        </div>
+      <div class="apply">
+        <btn class="with-bg--primary">立即体验
+          <icon :path="mdiCreationOutline"/>
+        </btn>
+        <btn class="without-bg--primary" href="https://seati.cc">打开官网
+          <icon :path="mdiLaunch"/>
+        </btn>
+      </div>
+
+      <section class="section__here_you_can">
+        <h2>免费、灵活、开放</h2>
+      </section>
     </div>
+  </div>
 </template>
 
 <style lang="less" scoped>
 .hello-card {
 
-    padding: 40px 0;
+  padding: 40px 0;
 
-    h1 {
-        font-size: 60px;
-        margin: 16px 0;
-    }
+  h1 {
+    font-size: 60px;
+    margin: 16px 0;
+  }
 
-    p {
-        font-size: 24px;
-    }
+  p {
+    font-size: 24px;
+  }
+
+  .apply {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+  }
 }
 </style>
 
