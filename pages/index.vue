b178e39f723431576d1376b90fb3af459617464b
Subilan <suyuanliang1995@gmail.com>
2024-05-18 21:15:55 +0800

    chore: add user basic

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="page-index">
-        
+    <div class="page-index container">
     </div>
 </template>
+
+<script lang="ts" setup>
+import getUsername from "~/utils/getUsername";
+import {BackendCodes} from "~/consts";
+
+let user = reactive<User>({
+  id: 0,
+  username: '',
+  nickname: '',
+  email: '',
+  mcid: '',
+  createdAt: 0,
+  updatedAt: 0
+})
+const username = getUsername();
+
+async function initUser() {
+  const userResult = await get<UserResp>(`/api/user/profile/${username.value}`);
+  if (userResult.code !== BackendCodes.OK) {
+
+  } else {
+    user = userResult.data;
+  }
+}
+
+onMounted(() => {
+  initUser();
+})
+</script>
--- types/index.ts
+++ types/index.ts
@@ -8,9 +8,21 @@ declare global {
         token: string
     }>;
 
+    type UserResp = Resp<User>
+
     interface Resp<T> {
         code: number,
         msg: string,
         data: T
+    }
+
+    interface User {
+        id: number
+        username: string,
+        nickname: string,
+        email: string
+        mcid: string,
+        createdAt: number,
+        updatedAt: number
     }
 }
