4bc0a708f0a723feee9ec08589969bdf5d5e8120
Subilan <suyuanliang1995@gmail.com>
2024-05-18 19:02:11 +0800

    chore: add token to post header

--- utils/post.ts
+++ utils/post.ts
@@ -1,7 +1,12 @@
+import {useLocalStorage} from "@vueuse/core";
+
 export default async function <T>(url: string, body: object): Promise<T> {
     return await $fetch<T>(url, {
         method: 'post',
         body,
-        ignoreResponseError: true
+        ignoreResponseError: true,
+        headers: {
+            'Token': useLocalStorage('tisea-auth-token', '').value
+        }
     });
 }
