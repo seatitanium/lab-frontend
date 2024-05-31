8256dfd7662800080edb717004c617e5d41d0b9f
Subilan <suyuanliang1995@gmail.com>
2024-05-31 22:51:54 +0800

    chore: add card display title

--- components/card-title.vue
+++ components/card-title.vue
@@ -20,5 +20,16 @@ h1.card-title {
     width: 32px;
     height: 32px;
   }
+
+  &.display {
+    font-size: 18px;
+    font-weight: 500;
+    color: @primaryd;
+
+    svg {
+      width: 20px;
+      height: 20px;
+    }
+  }
 }
 </style>
