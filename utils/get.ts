4122fcb295ec301849c4823f02a14d528cc1008f
Subilan <suyuanliang1995@gmail.com>
2024-05-31 22:52:04 +0800

    fix: optional header

--- utils/get.ts
+++ utils/get.ts
@@ -1,11 +1,11 @@
 import {useLocalStorage} from "@vueuse/core";
 
-export default async function <T>(url: string): Promise<T> {
+export default async function <T>(url: string, withToken = true): Promise<T> {
     return await $fetch<T>(url, {
         method: 'get',
         ignoreResponseError: true,
-        headers: {
+        headers: withToken ? {
             'Token': useLocalStorage('tisea-auth-token', '').value
-        }
+        } : {}
     });
 }
