426c16497ec076ac2ea2af7f0f89eb19dccc16ce
Subilan <suyuanliang1995@gmail.com>
2024-07-12 00:52:40 +0800

    chore: add `block` component

---
+++ components/block.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="block">
+    <slot/>
+  </div>
+</template>
+
+<style lang="less">
+@import "@/assets/var.less";
+.block {
+  border-radius: 10px;
+  padding: 16px;
+  margin: 16px 0;
+  display: flex;
+  align-items: center;
+  gap: 16px;
+
+  &.narrow {
+    padding: 8px 16px;
+  }
+
+  &.with-bg--primary {
+    background: @primaryl;
+    color: @primaryd;
+  }
+}
+</style>
