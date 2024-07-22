4e298f363bbe13534a9265986dc7c3b432ae447a
Subilan <suyuanliang1995@gmail.com>
2024-07-22 22:49:23 +0800

    chore: shadow tuning

--- components/card.vue
+++ components/card.vue
@@ -9,7 +9,7 @@
 
 .card {
   padding: 30px 20px;
-  box-shadow: 0 1px 2px rgba(0, 0, 0, .3);
+  box-shadow: 0 1.5px 3px rgba(0, 0, 0, .3);
   border-radius: 20px;
   position: relative;
   overflow: hidden;
