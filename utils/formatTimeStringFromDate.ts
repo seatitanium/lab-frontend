a15815fbbbd40e44cf4e66b7059e770e063e731e
Subilan <suyuanliang1995@gmail.com>
2024-07-24 10:53:04 +0800

    fix: month

--- utils/formatTimeStringFromDate.ts
+++ utils/formatTimeStringFromDate.ts
@@ -1,5 +1,5 @@
 import withLeadingZero from "~/utils/withLeadingZero";
 
 export default function (date: Date) {
-    return `${date.getFullYear()}-${withLeadingZero(date.getMonth())}-${withLeadingZero(date.getDate())} ${withLeadingZero(date.getHours())}:${withLeadingZero(date.getMinutes())}:${withLeadingZero(date.getSeconds())}`;
+    return `${date.getFullYear()}-${withLeadingZero(date.getMonth()+1)}-${withLeadingZero(date.getDate())} ${withLeadingZero(date.getHours())}:${withLeadingZero(date.getMinutes())}:${withLeadingZero(date.getSeconds())}`;
 }
--- utils/formatTimeStringFromString.ts
+++ utils/formatTimeStringFromString.ts
@@ -4,5 +4,5 @@ export default function (str: string) {
     // str can be standardized UTC+0 time.
     // new Date will turn the time to local time.
     const date = new Date(str);
-    return `${date.getFullYear()}-${withLeadingZero(date.getMonth())}-${withLeadingZero(date.getDate())} ${withLeadingZero(date.getHours())}:${withLeadingZero(date.getMinutes())}:${withLeadingZero(date.getSeconds())}`;
+    return `${date.getFullYear()}-${withLeadingZero(date.getMonth()+1)}-${withLeadingZero(date.getDate())} ${withLeadingZero(date.getHours())}:${withLeadingZero(date.getMinutes())}:${withLeadingZero(date.getSeconds())}`;
 }
