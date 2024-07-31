ffd573812211381f734e2c890aa39a44627f623e
Subilan <suyuanliang1995@gmail.com>
2024-08-01 00:20:32 +0800

    chore: add `del` name

--- utils/del.ts
+++ utils/del.ts
@@ -1,7 +1,7 @@
 import {useLocalStorage} from "@vueuse/core";
 import {APIEndpoint} from "~/consts";
 
-export default async function <T>(url: string, withToken = true): Promise<Resp<T>> {
+export default async function del<T>(url: string, withToken = true): Promise<Resp<T>> {
     return await $fetch<Resp<T>>(APIEndpoint + url, {
         method: 'delete',
         ignoreResponseError: true,
