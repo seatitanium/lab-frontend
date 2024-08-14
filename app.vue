eb6a3a45c4c0132b66977b345b7f75198d7b5623
Subilan <suyuanliang1995@gmail.com>
2024-08-14 21:24:32 +0800

    chore: text

--- app.vue
+++ app.vue
@@ -7,9 +7,9 @@
       <div class="not-enough-width-warn" v-else>
         <div class="txt">
           <h2>显示不兼容</h2>
-          <p>你的屏幕不足以显示 Lab 的界面。请在宽度<strong>大于或等于 1100px</strong>、高度<strong>大于或等于 850px</strong> 的设备或窗口上访问本页面。1500px 以上宽度访问效果最佳。</p>
+          <p>你的屏幕不足以显示 Lab 的界面。请在宽度<strong>大于或等于 1100px</strong>、高度<strong>大于或等于 850px</strong> 的设备或窗口上访问本页面。1500px 以上宽度、1000px 以上高度访问效果最佳。</p>
           <p>通常情况下，1080p 及以上的显示器都可以满足该要求。</p>
-          <p>若屏幕分辨率已满足，请将窗口横向拉伸，直至页面可以正常显示。</p>
+          <p>若屏幕分辨率已满足，请将窗口拉伸，直至页面可以正常显示。</p>
           <p><a target="_blank" href="https://www.baidu.com/s?wd=1920x1080">点此 - 在百度上了解更多关于分辨率的信息</a></p>
         </div>
       </div>
