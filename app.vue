4e0b02df21b99755e1402141eea5bbf4e9828f4f
Subilan <suyuanliang1995@gmail.com>
2024-08-15 22:11:54 +0800

    fix: 500

--- app.vue
+++ app.vue
@@ -4,18 +4,21 @@
       <NuxtPage/>
     </NuxtLayout>
   </div>
-  <modal v-model="screenNotQualifiedModal" :class="narrowScreenQualificationModal ? 'narrow' : ''" class="describe with-bg--blurred with-bg--darken" :allow-esc="false">
+  <modal v-model="screenNotQualifiedModal" :class="narrowScreenQualificationModal ? 'narrow' : ''"
+         class="describe with-bg--blurred with-bg--darken" :allow-esc="false">
     <modal-content>
       <icon color="#ff9800" :path="mdiMonitorOff"/>
       <h2>不支持的屏幕尺寸</h2>
-      <p>当前你的浏览器显示尺寸为 {{ windowSize.width }}*{{ windowSize.height}} px<sup>2</sup>。Lab 需要至少 {{ widthThreshold }}*{{ heightThreshold }} px<sup>2</sup> 的空间来正常显示。</p>
+      <p>当前你的浏览器显示尺寸为 {{ windowSize.width }}*{{ windowSize.height }} px<sup>2</sup>。Lab 需要至少
+        {{ widthThreshold }}*{{ heightThreshold }} px<sup>2</sup> 的空间来正常显示。</p>
     </modal-content>
     <modal-actions class="right">
       <btn class="with-bg--white hover--dim" @click="screenMoreInformationPopup = true">了解更多</btn>
     </modal-actions>
   </modal>
   <anywhere-popup v-model="screenMoreInformationPopup" :code="false">
-    <p>浏览器的显示尺寸与屏幕本身的<strong>硬件</strong>参数和系统相关的<strong>软件</strong>参数均有联系，此处提示屏幕尺寸不受支持，则是指客观上屏幕当前的缩放无法正常容纳页面布局。</p>
+    <p>浏览器的显示尺寸与屏幕本身的<strong>硬件</strong>参数和系统相关的<strong>软件</strong>参数均有联系，此处提示屏幕尺寸不受支持，则是指客观上屏幕当前的缩放无法正常容纳页面布局。
+    </p>
     <p><strong>Lab 目前不支持在手机上使用。</strong>我们将在未来推出面向手机的更加轻便的版本。</p>
   </anywhere-popup>
 </template>
@@ -45,12 +48,14 @@ watch(windowSizeReactive, windowSize => {
 });
 
 watch(screenNotQualifiedModal, v => {
-  if (v) {
-    document.body.style.height = '100%';
-    document.body.style.overflow = 'hidden';
-  } else {
-    document.body.style.height = '';
-    document.body.style.overflow = '';
+  if (document) {
+    if (v) {
+      document.body.style.height = '100%';
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.height = '';
+      document.body.style.overflow = '';
+    }
   }
 }, {
   immediate: true
