0e5ed05ad78f5c1c2f2a52346251f3d4146fd7af
Subilan <suyuanliang1995@gmail.com>
2024-08-27 10:58:18 +0800

    chore: valid time board

--- pages/board.vue
+++ pages/board.vue
@@ -19,51 +19,16 @@
     </section>
     <section>
       <transition name="flow">
-        <card class="narrow" v-if="loginRecordBoard.length > 0">
-          <card-label>
-            <icon :path="mdiLogin"/>
-            登入次数
-          </card-label>
-          <card-content>
-            <table>
-              <thead>
-              <tr>
-                <th>名次 RANK</th>
-                <th v-for="x in Object.values(loginBoardTableHeads)">{{ x }}</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr v-for="(x, i) in loginRecordBoard">
-                <td>
-                  <div class="cell">
-                    {{ i + 1 }}
-                  </div>
-                </td>
-                <td v-for="y in Object.keys(loginBoardTableHeads)">
-                  <div class="cell">
-                    <player-avatar v-if="y === 'player'" :name="x[y]"/>
-                    {{ y === 'lastCreatedAt' ? formatTimeStringFromString(x[y]) : x[y] }}
-                  </div>
-                </td>
-              </tr>
-              </tbody>
-            </table>
-          </card-content>
-        </card>
-      </transition>
-    </section>
-    <section>
-      <transition name="flow">
-        <card class="narrow" style="transition-delay: .2s" v-if="playtimeBoard.length > 0">
+        <card class="narrow" v-if="playtimeBoard.length > 0">
           <card-label>
             <icon :path="mdiClockOutline"/>
-            游玩时长
+            有效时长
           </card-label>
           <card-content>
             <table>
               <thead>
               <tr>
-                <th>名次 RANK</th>
+                <th>名次</th>
                 <th v-for="x in Object.values(playtimeBoardTableHeads)">{{ x }}</th>
               </tr>
               </thead>
@@ -87,6 +52,41 @@
         </card>
       </transition>
     </section>
+    <section>
+      <transition name="flow">
+        <card class="narrow" style="transition-delay: .2s" v-if="loginRecordBoard.length > 0">
+          <card-label>
+            <icon :path="mdiLogin"/>
+            登入次数
+          </card-label>
+          <card-content>
+            <table>
+              <thead>
+              <tr>
+                <th>名次</th>
+                <th v-for="x in Object.values(loginBoardTableHeads)">{{ x }}</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr v-for="(x, i) in loginRecordBoard">
+                <td>
+                  <div class="cell">
+                    {{ i + 1 }}
+                  </div>
+                </td>
+                <td v-for="y in Object.keys(loginBoardTableHeads)">
+                  <div class="cell">
+                    <player-avatar v-if="y === 'player'" :name="x[y]"/>
+                    {{ y === 'lastCreatedAt' ? formatTimeStringFromString(x[y]) : x[y] }}
+                  </div>
+                </td>
+              </tr>
+              </tbody>
+            </table>
+          </card-content>
+        </card>
+      </transition>
+    </section>
   </div>
 </template>
 
@@ -96,16 +96,16 @@ import {BackendCodes, PeriodTag} from "~/consts";
 import {definePageMeta} from "#imports";
 
 const loginBoardTableHeads = {
-  'player': '玩家 PLAYER',
-  'count': '登入次数 LOGIN',
-  'lastCreatedAt': '最后登入时间 LAST_LOGIN'
+  'player': '玩家',
+  'count': '登入次数',
+  'lastCreatedAt': '最后登入时间'
 }
 
 const playtimeBoardTableHeads = {
-  'player': '玩家 PLAYER',
-  'timeTotal': '总游玩时长 PLAYTIME_TOTAL',
-  'timeAfk': '挂机时长 PLAYTIME_AFK',
-  'timeValid': '有效时长 PLAYTIME_VALID',
+  'player': '玩家',
+  'timeValid': '有效时长',
+  'timeTotal': '总游玩时长',
+  'timeAfk': '挂机时长',
 }
 
 const loginRecordBoard = reactive<LoginRecordBoard[]>([]);
